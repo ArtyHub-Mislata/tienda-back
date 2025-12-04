1c7654306eb57990b0913d3e651834b0be129e10
luismi <olmedaromeroluismiguel@gmail.com>
2025-12-04 09:47:58 +0100

    fix(ArtyHub-Mislata/sprints#3): Arreglada la implementación del page

--- src/main/java/es/artyhub/tienda_back/domain/service/impl/CategoryServiceImpl.java
+++ src/main/java/es/artyhub/tienda_back/domain/service/impl/CategoryServiceImpl.java
@@ -1,6 +1,6 @@
 package es.artyhub.tienda_back.domain.service.impl;
 
-import org.springframework.data.domain.Page;
+import es.artyhub.tienda_back.domain.model.Page;
 import es.artyhub.tienda_back.domain.dto.CategoryDto;
 import es.artyhub.tienda_back.domain.exception.BusinessException;
 import es.artyhub.tienda_back.domain.repository.CategoryRepositoy;
