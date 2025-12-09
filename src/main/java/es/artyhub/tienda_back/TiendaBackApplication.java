8679f8bc794fa556025725c1cd4f0e4b1baaa9d6
jaime <jaimejgc2018@gmail.com>
2025-12-09 20:58:42 +0100

    Mini commit

--- src/main/java/es/artyhub/tienda_back/TiendaBackApplication.java
+++ src/main/java/es/artyhub/tienda_back/TiendaBackApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TiendaBackApplication {
 
 	public static void main(String[] args) {
+		//A
 		SpringApplication.run(TiendaBackApplication.class, args);
 	}
 
