0698f382be7ef6b7dd3f65f775aa6d3dff6986db
luismi <olmedaromeroluismiguel@gmail.com>
2025-12-18 09:15:00 +0100

    fix(ArtyHub_Mislata/sprints#6): Añadidas otras imágenes

--- src/main/resources/db/migration/V2__sample_inserts.sql
+++ src/main/resources/db/migration/V2__sample_inserts.sql
@@ -45,8 +45,8 @@ INSERT INTO artworks (
       ('Luces de Calle', 'Fotografía nocturna urbana', 'https://images.unsplash.com/photo-1501594907352-04cda38ebc29?fit=crop&w=400&h=300', 120.00, 3, 4),
       ('Sombras y Contrastes', 'Fotografía en blanco y negro', 'https://images.unsplash.com/photo-1462331940025-496dfbfc7564?fit=crop&w=400&h=300', 140.00, 3, 4),
 
-      ('Mundo Pixelado', 'Ilustración digital futurista', 'https://www.freepik.es/vector-premium/pixel-tierra-miniatura-8-bits-mundo-ilustraciones-vectoriales-globo-terraqueo-arte-pixeles_24430109.htm', 200.00, 4, 5),
-      ('Geometría Viva', 'Arte digital geométrico', 'https://www.freepik.es/vectores/geometria-viva/2', 180.00, 4, 5),
+      ('Mundo Pixelado', 'Ilustración digital futurista', 'https://unsplash.com/es/fotos/un-juguete-de-vaca-marron-de-minecraft-sobre-un-fondo-negro-oYnzLuCRCsM', 200.00, 4, 5),
+      ('Geometría Viva', 'Arte digital geométrico', 'https://unsplash.com/es/fotos/fotografia-de-angulo-bajo-de-un-edificio-de-hormigon-durante-el-dia-IsAq-hI1HAo', 180.00, 4, 5),
 
-      ('Personaje Fantástico', 'Ilustración de fantasía épica', 'https://www.xataka.com/n/mejores-desconocidas-obras-fantasia-epica-contemporanea', 95.00, 5, 2),
-      ('Naturaleza Abstracta', 'Composición ilustrada moderna', 'https://pixers.es/fotomurales/naturaleza-abstracta-27992187', 110.00, 5, 5);
+      ('Personaje Fantástico', 'Ilustración de fantasía épica', 'https://unsplash.com/es/fotos/UzS3sGcLK2w', 95.00, 5, 2),
+      ('Naturaleza Abstracta', 'Composición ilustrada moderna', 'https://unsplash.com/es/fotos/formacion-rocosa-marron-bajo-nubes-blancas-durante-el-dia-RYtiT3b7XW4', 110.00, 5, 5);
